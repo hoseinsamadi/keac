f5dd0672218ca850560054bf3705fc956039bfc4
BiBak <Husein.samadi@gmail.com>
2024-10-20 15:40:32 +0330

    added app and config views and urls keac

--- keac/views.py
+++ keac/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
-# Create your views here.
+def http_index(request):
+    return HttpResponse('<h1>hosein</h1>') 
